fb01983546bfe78dc6ebcaafb8950efdd147512d
shibis17 <shibis17@gmail.com>
2020-07-19 15:02:24 +0530

    search bar made as a separate component

---
+++ screens/Components/SearchBar.js
@@ -0,0 +1,147 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
+
+const SearchBar = props => {
+
+    const [searchText, setSearchText] = useState('');
+    const [searchHistory, setSearchHistory] = useState([]);
+    const [displayContent, setDisplayContent] = useState(false);
+
+    const searchTextHandler = (searchValue) => {
+        setSearchText(searchValue);
+    }
+
+    const SearchIconPressHandler = (enteredSearch) => {
+        if (searchHistory.length >= 7) {
+            searchHistory.shift();
+        }
+        if (/\S/.test(searchText) && searchText != null) {
+            setSearchHistory([...searchHistory, { id: Math.random().toString(), value: enteredSearch }]);
+        }
+        else
+            return;
+    }
+
+    const DisplayContentHandler = () => {
+        setDisplayContent(true);
+    }
+
+    const FillSearchTextHandler = (selectedText) => {
+        setSearchText(selectedText);
+    }
+
+    return (
+        <View>
+            <View style={styles.headerContainer}>
+                <View style={styles.inputContainer}>
+                    <TouchableWithoutFeedback onPress={DisplayContentHandler}>
+                        <Text style={styles.input}>Search for Products...</Text>
+                    </TouchableWithoutFeedback>
+                    <TouchableOpacity onPress={DisplayContentHandler} >
+                        <Image source={require('../../assets/images/search.png')} style={styles.searchIcon} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <Modal visible={displayContent} onRequestClose={() => {
+                setDisplayContent(false);
+                setSearchText('');
+            }}>
+                <View style={styles.inputTextContainer}>
+                    <TextInput placeholder='Search for products...' autoFocus onChangeText={searchTextHandler} value={searchText} style={styles.inputText} />
+                    <TouchableOpacity onPress={SearchIconPressHandler.bind(this, searchText)}>
+                        <Image source={require('../../assets/images/search.png')} style={styles.searchIcon} />
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <FlatList
+                        keyboardShouldPersistTaps='always'
+                        data={searchHistory}
+                        renderItem={itemData => (
+                            <View style={styles.ListContainer}>
+                                <Text style={styles.list}>{itemData.item.value}</Text>
+                                <TouchableOpacity onPress={FillSearchTextHandler.bind(this, itemData.item.value)}>
+                                    <Image source={require('../../assets/images/arrow.png')} style={styles.arrowIcon} />
+                                </TouchableOpacity>
+                            </View>
+                        )}
+                    />
+                </View>
+            </Modal>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    headerContainer:{
+        width: '100%',
+        backgroundColor: '#ec2F4B',
+        padding: 0,
+      },
+  
+      inputContainer:{
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        padding:5,
+        marginLeft:5
+      },
+  
+      searchIcon:{
+        height: 10,
+        width: 10,
+        padding: 15,
+        paddingBottom: 10
+      },
+  
+      arrowIcon:{
+        height: 10,
+        width: 10,
+        padding: 10,
+      },
+
+      input:{
+        marginVertical: 10,
+        padding: 10,
+        paddingLeft: 10,
+        marginRight: 10,
+        width: '90%',
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: 'black',
+        backgroundColor: 'white',
+        color: 'gray'
+      },
+  
+      inputTextContainer:{
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        padding: 15,
+      },
+  
+      inputText:{
+        marginVertical: 10,
+        padding: 5,
+        paddingLeft: 10,
+        marginRight: 10,
+        width: '90%',
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: 'black',
+        backgroundColor: 'white'
+      },
+  
+      ListContainer:{
+        flexDirection: 'row',
+        backgroundColor: '#EEE',
+        marginHorizontal: 15,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingLeft: 30,
+        padding: 15,
+        borderBottomWidth: 1,
+        borderBottomColor: 'black',
+      },
+});
+
+export default SearchBar;
--- screens/Shibi.js
+++ screens/Shibi.js
@@ -1,7 +1,8 @@
 import React,{ useState } from 'react';
-import { Text, View, StyleSheet, ScrollView, TextInput, Image, TouchableOpacity, Modal, TouchableWithoutFeedback, FlatList } from 'react-native';
+import { View, StyleSheet, ScrollView, TextInput, Image, TouchableOpacity, Modal, TouchableWithoutFeedback, FlatList } from 'react-native';
 import { SliderBox } from 'react-native-image-slider-box';
 import Card from './Components/Card'
+import SearchBar from './Components/SearchBar';
 
 export default function Shibi()
 {
@@ -57,49 +58,11 @@ export default function Shibi()
   }
   ]);
 
-  const [searchText, setSearchText] = useState('');
-  const [searchHistory, setSearchHistory] = useState([]);
-  const [displayContent, setDisplayContent] = useState(false);
-
-  const searchTextHandler = (searchValue) => {
-    setSearchText(searchValue);
-  }
-
-  const SearchIconPressHandler = (enteredSearch) => {
-      if(searchHistory.length >= 7)
-      {
-        searchHistory.shift();
-      }
-      if(/\S/.test(searchText) && searchText != null)
-      {
-        setSearchHistory([...searchHistory, {id:Math.random().toString(), value:enteredSearch}]);   
-      }
-      else
-        return;
-  }
-
-  const DisplayContentHandler = () =>{
-    setDisplayContent(true);
-  }
-
-  const FillSearchTextHandler = (selectedText) =>{
-    setSearchText(selectedText);
-  }
-
   return(
       <View style={styles.screen}>
-        <View style={styles.headerContainer}>        
-          <View style={styles.inputContainer}>
-            <TouchableWithoutFeedback onPress={DisplayContentHandler}>
-              <Text style={styles.input}>Search for Products...</Text>
-            </TouchableWithoutFeedback>
-            <TouchableOpacity onPress={DisplayContentHandler} >
-              <Image source={require('../assets/images/search.png')} style={styles.searchIcon}/>
-            </TouchableOpacity>
-          </View>
-        </View>
+        <SearchBar />
         <ScrollView>
-          <View style={styles.imageDeckContainer}>
+          <View>
             <View style={styles.imageDeck}>
               <SliderBox
                 images={imageDeckArray}
@@ -116,31 +79,6 @@ export default function Shibi()
             <Card images={imageCardArray2} header='Mobile Phones' image1={require('../assets/images/offerphone1.jpeg')} image2={require('../assets/images/offerphone2.jpeg')}/>
           </View>
         </ScrollView>
-        <Modal visible={displayContent} onRequestClose={()=>{
-            setDisplayContent(false);
-            setSearchText('');
-          }}>
-            <View style={styles.inputTextContainer}>
-              <TextInput placeholder='Search for products...' autoFocus onChangeText={searchTextHandler} value={searchText} style={styles.inputText}/>
-              <TouchableOpacity onPress={SearchIconPressHandler.bind(this,searchText)}>
-                <Image source={require('../assets/images/search.png')} style={styles.searchIcon}/>
-              </TouchableOpacity>
-            </View>
-            <View>
-              <FlatList
-                  keyboardShouldPersistTaps='always'
-                  data={searchHistory}
-                  renderItem={itemData => (
-                    <View style={styles.ListContainer}>
-                      <Text style={styles.list}>{itemData.item.value}</Text>
-                      <TouchableOpacity onPress={FillSearchTextHandler.bind(this, itemData.item.value)}>
-                        <Image source={require('../assets/images/arrow.png')} style={styles.arrowIcon}/>
-                      </TouchableOpacity>
-                    </View>
-                  )}
-              />
-            </View>
-          </Modal>
       </View>
   );  
 }
@@ -149,78 +87,6 @@ const styles = StyleSheet.create({
     screen:{
       paddingTop: 0,
       flex: 1,
-    },
-
-    headerContainer:{
-      width: '100%',
-      backgroundColor: '#ec2F4B',
-      padding: 0,
-    },
-
-    inputContainer:{
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-      padding:5,
-      marginLeft:5
-    },
-
-    searchIcon:{
-      height: 10,
-      width: 10,
-      padding: 15,
-      paddingBottom: 10
-    },
-
-    arrowIcon:{
-      height: 10,
-      width: 10,
-      padding: 10,
-    },
-
-    input:{
-      marginVertical: 10,
-      padding: 10,
-      paddingLeft: 10,
-      marginRight: 10,
-      width: '90%',
-      borderWidth: 1,
-      borderRadius: 10,
-      borderColor: 'black',
-      backgroundColor: 'white',
-      color: 'gray'
-    },
-
-    inputTextContainer:{
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-      padding: 15,
-    },
-
-    inputText:{
-      marginVertical: 10,
-      padding: 5,
-      paddingLeft: 10,
-      marginRight: 10,
-      width: '90%',
-      borderWidth: 1,
-      borderRadius: 10,
-      borderColor: 'black',
-      backgroundColor: 'white'
-    },
-
-
-    ListContainer:{
-      flexDirection: 'row',
-      backgroundColor: '#EEE',
-      marginHorizontal: 15,
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      paddingLeft: 30,
-      padding: 15,
-      borderBottomWidth: 1,
-      borderBottomColor: 'black',
     },
 
     imageDeck:{     
