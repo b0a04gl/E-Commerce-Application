9a5298d5be2e42eae5cb25aa389a17fbf2d15480
shibis17 <shibis17@gmail.com>
2020-07-30 21:46:28 +0530

    Added Image for products to dealers and implemented with userToken

--- screens/DealerProducts.js
+++ screens/DealerProducts.js
@@ -4,14 +4,13 @@ import * as firebase from 'firebase';
 import ApiKeys from '../database/RealtimeDb';
 import { FlatList } from 'react-native-gesture-handler';
 import AsyncStorage from '@react-native-community/async-storage';
-
+import * as ImagePicker from 'expo-image-picker';
 
 export default class DealerProducts extends React.Component {
   _isMounted = false;
   constructor(props) {
     super(props);
     this.state = {
-      dealerID: '002',
       dealerProducts: [],
       productName: '',
       productPrice: '',
@@ -19,52 +18,30 @@ export default class DealerProducts extends React.Component {
       showModal: false,
       buttonTitle: 'Add Product',
       index: -1,
-      userToken : null,
+      userToken: null,
+      image: require('../assets/images/add.png'),
     };
     if (!firebase.apps.length) {
       firebase.initializeApp(ApiKeys.firebaseConfig);
     }
-
-    
-    setTimeout(async() => {
-     
-      let token;
-      token = null;
-      
-      try {
-        token = await AsyncStorage.getItem('userToken');
-        
-      } catch(e) {
-        console.log(e);
-      }
-
-      this.setState(
-        {
-          userToken : token,
-        }
-      );
-
-      Alert.alert("current user : "+this.state.userToken);
-      
-    }, 100);
- 
-
-
-
-
-    
-
   }
 
   componentDidMount() {
-    this._isMounted = true;
-    firebase.database().ref('/dealers/' + this.state.dealerID).on('value', data => {
-      if (this._isMounted) {
-        if (data.val()) {
-          this.setState({
-            dealerProducts: data.val(),
-          });
-        }
+    AsyncStorage.getItem('userToken').then((userToken) => {
+      if (userToken) {
+        this.setState({
+          userToken: userToken,
+        });
+        this._isMounted = true;
+        firebase.database().ref('/dealers/' + this.state.userToken).on('value', data => {
+          if (this._isMounted) {
+            if (data.val()) {
+              this.setState({
+                dealerProducts: data.val(),
+              });
+            }
+          }
+        });
       }
     });
   }
@@ -80,29 +57,67 @@ export default class DealerProducts extends React.Component {
     });
   };
 
+  AddImageHandler = async () => {
+    const permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+    let URI = pickerResult.uri;
+    const imageName = URI.substring(URI.lastIndexOf('/') + 1);
+    const response = await fetch(URI);
+    const blob = await response.blob();
+    firebase
+      .storage()
+      .ref(imageName)
+      .put(blob)
+      .then((snapshot) => {
+        console.log(`${imageName} has been successfully uploaded.`);
+        snapshot.ref.getDownloadURL().then((url) => {
+          this.setState({
+            image: { uri: url },
+          });
+        });
+      })
+      .catch((e) => console.log('uploading image error => ', e));
+  };
+
   addProduct = () => {
-    const product = {
-      productName: this.state.productName,
-      productPrice: this.state.productPrice,
-      stocks: this.state.stocks,
-      status: 'Pending'
-    };
-    let products;
-    if (this.state.buttonTitle === 'Add Product') {
-      products = [...this.state.dealerProducts, product];
+    if (this.state.image.uri) {
+      const product = {
+        productName: this.state.productName,
+        productPrice: this.state.productPrice,
+        stocks: this.state.stocks,
+        status: 'Pending',
+        image: this.state.image,
+      };
+      let products;
+      if (this.state.buttonTitle === 'Add Product') {
+        products = [...this.state.dealerProducts, product];
+      }
+      else {
+        products = this.state.dealerProducts;
+        products.splice(this.state.index, 1);
+        products.push(product);
+      }
+      this.setState({
+        dealerProducts: products,
+        productName: '',
+        productPrice: '',
+        stocks: '',
+        showModal: false,
+        image: require('../assets/images/add.png'),
+      });
     }
     else {
-      products = this.state.dealerProducts;
-      products.splice(this.state.index, 1);
-      products.push(product);
+      Alert.alert('Image Not Found', 'Pick an Image from the gallery');
     }
-    this.setState({
-      dealerProducts: products,
-      productName: '',
-      productPrice: '',
-      stocks: '',
-      showModal: false,
-    });
   };
 
   updateProduct = (index) => {
@@ -127,11 +142,11 @@ export default class DealerProducts extends React.Component {
   };
 
   saveToDB = () => {
-    if(this.state.dealerProducts.length > 0) {
-      firebase.database().ref('/dealers/'+this.state.dealerID).set(this.state.dealerProducts).then(() => {
+    if (this.state.dealerProducts.length > 0) {
+      firebase.database().ref('/dealers/' + this.state.userToken).set(this.state.dealerProducts).then(() => {
         console.log('Inserted');
       }).catch((error) => {
-          console.log(error);
+        console.log(error);
       });
     }
   }
@@ -150,9 +165,6 @@ export default class DealerProducts extends React.Component {
 
   render() {
     return (
-
-
-      
       <View style={styles.screen}>
         <View style={styles.row}>
           <Text style={styles.text}>Products List</Text>
@@ -163,7 +175,7 @@ export default class DealerProducts extends React.Component {
         <FlatList data={this.state.dealerProducts}
           renderItem={data => (
             <View style={styles.listContainer}>
-              <Image source={require('../assets/images/add.png')} style={styles.listimage} />
+              <Image source={data.item.image} style={styles.listimage} />
               <View style={styles.list}>
                 <Text style={styles.name}>{data.item.productName}</Text>
                 <Text style={styles.price}>{data.item.productPrice}</Text>
@@ -188,6 +200,9 @@ export default class DealerProducts extends React.Component {
           onRequestClose={this.closeModal}>
           <View style={styles.modalContainer}>
             <View style={styles.modalScreen}>
+              <TouchableOpacity onPress={this.AddImageHandler} style={styles.imageContainer}>
+                <Image source={this.state.image} style={styles.previewImage} />
+              </TouchableOpacity>
               <TextInput
                 placeholder='Enter your product name'
                 placeholderTextColor='black'
@@ -289,6 +304,17 @@ const styles = StyleSheet.create({
   modalScreen: {
     height: 400,
     width: '90%',
+  },
+
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+
+  previewImage: {
+    height: 80,
+    width: 80,
   },
 
   textInput: {
